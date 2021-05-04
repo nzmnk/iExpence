e9e3c12298c305f1be9e341a2875323366d1c0cc
Nikita Zimenko <nic.zimen@gmail.com>
2021-05-04 23:06:25 +0300

    Initial commit

---
+++ iExpence/iExpence/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpence
+//
+//  Created by Никита Зименко on 04.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpence/iExpence/iExpenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenceApp.swift
+//  iExpence
+//
+//  Created by Никита Зименко on 04.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
